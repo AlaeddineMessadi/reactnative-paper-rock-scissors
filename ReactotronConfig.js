65bca5ac3002542f44aa2199c1b04d5d0e83c1b0
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2021-01-30 23:51:03 +0100

    ReactotronConfig

---
+++ ReactotronConfig.js
@@ -0,0 +1,39 @@
+import Reactotron from 'reactotron-react-native'
+import { AsyncStorage } from "react-native";
+  
+Reactotron
+  .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
+  .configure({
+    name: "Paper Rock Scissors"
+  }) // controls connection & communication settings
+  .useReactNative({
+        asyncStorage: false,
+    networking: {
+      ignoreUrls: /symbolicate|127.0.0.1/,
+    },
+    editor: false,
+    errors: { veto: stackFrame => false },
+    overlay: false,
+  }) // add all built-in react native plugins
+  .connect() // let's connect!
+
+
+
+
+
+
+// Reactotron
+//   .setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
+//   .configure({
+//     name: "React Native Demo"
+//   })
+//   .useReactNative({
+//     asyncStorage: false, // there are more options to the async storage.
+//     networking: { // optionally, you can turn it off with false.
+//       ignoreUrls: /symbolicate/
+//     },
+//     editor: false, // there are more options to editor
+//     errors: { veto: (stackFrame) => false }, // or turn it off with false
+//     overlay: false, // just turning off overlay
+//   })
+//   .connect();
